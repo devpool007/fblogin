cd5840440da7ca006991ff1339b09521d2f4cb02
devpool007 <sharmadevansh007777@gmail.com>
2017-05-12 13:55:38 +0530

    Initial Commit

---
+++ closureprac/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  closureprac
+//
+//  Created by Devansh Sharma on 12/05/17.
+//  Copyright © 2017 Devansh Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
